c4e38b299fc24d0a5b10808d66257f30e50e2bdd
Sanjaym-98 <122872856+Sanjaym-98@users.noreply.github.com>
2023-01-25 18:01:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const app = require('./App');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const fileupload = require('express-fileupload');
 
 dotenv.config();
 //connect to DB
 mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log('Successful Connected to DB')
 })
+app.use(fileupload({
+    useTempFiles:true
+}))
 
 app.listen(3004, () => console.log('Server is up at 3004 port'));
+
