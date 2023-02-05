931533965c5ce38369a2c04741b739f56d7d40a9
Sanjaym-98 <122872856+Sanjaym-98@users.noreply.github.com>
2023-02-05 23:18:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 const express= require("express");
-const formidable = require("express-formidable");
+
 const app=express();
 const cors = require("cors")
+const fileupload = require('express-fileupload');
 
-require ("dotenv").config();
-const cloudinary = require ("cloudinary").v2;
-if (typeof (process.env.CLOUDINARY_URL) === 'undefined') {
-    console.warn('!! cloudinary config is undefined !!');
-    console.warn('export CLOUDINARY_URL or set dotenv file');
-  } else {
-    // console.log('cloudinary config:');
-    // console.log(cloudinary.config());
-  }
-  console.log('--');
-
+// require ("dotenv").config();
+// const cloudinary = require ("cloudinary").v2;
+// if (typeof (process.env.CLOUDINARY_URL) === 'undefined') {
+//     console.warn('!! cloudinary config is undefined !!');
+//     console.warn('export CLOUDINARY_URL or set dotenv file');
+//   } else {
+//     // console.log('cloudinary config:');
+//     // console.log(cloudinary.config());
+//   }
+  console.log('-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --');
+  app.use(fileupload({
+    useTempFiles:true
+}))
   app.use(cors({
     origin:"*"
 }))
-app.use(formidable()); //choose file //streaming multipart parser
+// app.use(formidable());
 const datarouter=require("./routes");
 app.use("/posts",datarouter)
 module.exports=app;
