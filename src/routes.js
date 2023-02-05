937a3b82d2056329fd74442d7b00f799eb50ccd7
Sanjaym-98 <122872856+Sanjaym-98@users.noreply.github.com>
2023-02-05 23:20:30 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 const PostModel = require("./modelschema");
-const cloudinary = require("cloudinary")
+const cloudinary = require("cloudinary").v2
+
+cloudinary.config({ 
+    cloud_name: 'dh25stzxv', 
+    api_key: '879589417436149', 
+    api_secret: 'mfQu-frn_Q17K6U6gYcph6dc05E' 
+  });
 
 router.get ("/", async(req, res)=>{
     try{
@@ -16,20 +22,25 @@ router.get ("/", async(req, res)=>{
 
 router.post("/", async(req,res)=>{
     try{
-        const imageurl = await cloudinary.uploader.upload(req.files.PostImage.path,{folder:"Home"});
-        let date = new Date();
+         // const imageurl = await cloudinary.uploader.upload(req.files.PostImage.path,{folder:"Home"});
+         const file= req.files.photo;
+         await cloudinary.uploader.upload(file.tempFilePath,async(err,result)=>{
+            console.log(result)
+            let date = new Date();
         const data= {
-            name: req.fields.name,
-            location: req.fields.location,
-            likes:req.fields.likes,
-            description:req.fields.description,
-            PostImage:imageurl.url,
+            name: req.body.name,
+            location: req.body.location,
+            likes:req.body.likes,
+            description:req.body.description,
+            PostImage:result.url,
             date: `${date.getFullYear()} / ${date.getMonth()} / ${date.getDate()}`,
         }
         const Post_Insert = await PostModel.create(data);
         res.status(200).json({
             message:Post_Insert,
         })
+         })
+        
     }
     catch(err){
         res.status(400).json({
